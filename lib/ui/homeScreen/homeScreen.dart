db991780bda02fb6fef12b7aa2accdc05e196ac9
robotoss <zoxo@outlook.com>
2019-10-08 23:38:54 +0600

    Add main theme

--- lib/ui/homeScreen/homeScreen.dart
+++ lib/ui/homeScreen/homeScreen.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:go_prague/theme/mainTheme.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        elevation: 0.0,
         centerTitle: true,
         title: Text(
           'Welcome in Go.Prague',
@@ -14,7 +16,21 @@ class HomeScreen extends StatelessWidget {
           ),
         ),
       ),
-      body: Container(),
+      body: Stack(
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            height: 130,
+            color: ColorPalette().mainBlack,
+            child: Text(
+              '123',
+              style: TextStyle(
+                color: Colors.white
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
