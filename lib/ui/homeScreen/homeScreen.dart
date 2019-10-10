3f23035d8742ea5180b91adcc1575a0f314fe452
robotoss <zoxo@outlook.com>
2019-10-10 23:23:38 +0600

    Add carousel to main Screen

--- lib/ui/homeScreen/homeScreen.dart
+++ lib/ui/homeScreen/homeScreen.dart
@@ -1,7 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:go_prague/data/models/toursData.dart';
+import 'package:go_prague/data/repository.dart';
 import 'package:go_prague/theme/mainTheme.dart';
+import 'package:go_prague/ui/widgets/carousel/carousel.dart';
 
 class HomeScreen extends StatelessWidget {
+  static List<Widget> elements = List();
+  static double carouselHeight = 160;
+
+  Future<List<ToursData>> getSlides(
+    BuildContext context,
+  ) async {
+    List<ToursData> _listData;
+    await Repository().accountTypes().then((toursList) {
+      _listData = toursList;
+      toursList.forEach((slide) {
+        elements.add(carouselItem(context, slide, carouselHeight));
+      });
+    });
+    return _listData;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,10 +29,7 @@ class HomeScreen extends StatelessWidget {
         centerTitle: true,
         title: Text(
           'Welcome in Go.Prague',
-          style: TextStyle(
-            fontSize: 32,
-            fontWeight: FontWeight.w700
-          ),
+          style: TextStyle(fontSize: 32, fontWeight: FontWeight.w700),
         ),
       ),
       body: Stack(
@@ -22,15 +38,89 @@ class HomeScreen extends StatelessWidget {
             width: double.infinity,
             height: 130,
             color: ColorPalette().mainBlack,
-            child: Text(
-              '123',
-              style: TextStyle(
-                color: Colors.white
-              ),
-            ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              FutureBuilder(
+                  future: getSlides(context),
+                  builder: (context, snapshot) {
+                    return snapshot.data != null
+                        ? MainCarousel(
+                            elements: elements,
+                            height: carouselHeight,
+                          )
+                        : loadData(context, carouselHeight);
+                  }),
+            ],
           )
         ],
       ),
     );
   }
+}
+
+Widget loadData(BuildContext context,  double carouselHeight){
+  return Container(
+    height: carouselHeight,
+    width: MediaQuery.of(context).size.width,
+    child: Center(
+      child: CircularProgressIndicator(),
+    ),
+  );
+}
+
+Widget carouselItem(BuildContext context, ToursData sliderData, double carouselHeight) {
+  return GestureDetector(
+    onTap: () {},
+    child: Container(
+      height: carouselHeight,
+      width: MediaQuery.of(context).size.width,
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 20),
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: NetworkImage(
+                  sliderData.imgUrls[0],
+                ),
+                fit: BoxFit.cover)),
+        child: Container(
+          color: Colors.black.withOpacity(0.1),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                sliderData.name,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 32,
+                    fontWeight: FontWeight.w700),
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              SizedBox(
+                height: 30,
+                width: 150,
+                child: FlatButton(
+                    onPressed: () {},
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                    color: ColorPalette().mainGreen,
+                    child: Text(
+                      'GO',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 22,
+                          fontWeight: FontWeight.w700),
+                    )),
+              )
+            ],
+          ),
+        ),
+      ),
+    ),
+  );
 }
