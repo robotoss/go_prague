45444431bcd3e7d72dbe3e7d56700c087f3d6bcb
robotoss <zoxo@outlook.com>
2019-10-12 00:42:21 +0600

    Add new type of carousel

--- lib/ui/widgets/carousel/carousel.dart
+++ lib/ui/widgets/carousel/carousel.dart
@@ -79,3 +79,81 @@ class _MainCarouselState extends State<MainCarousel> {
     );
   }
 }
+
+
+class InfoCarousel extends StatefulWidget {
+  final List elements;
+  final double height;
+
+  InfoCarousel({
+    this.elements,
+    this.height
+  });
+
+  @override
+  _InfoCarouselState createState() => _InfoCarouselState();
+}
+
+class _InfoCarouselState extends State<InfoCarousel> {
+  int _current = 0;
+
+  List<T> map<T>(List list, Function handler) {
+    List<T> result = [];
+    for (var i = 0; i < list.length; i++) {
+      result.add(handler(i, list[i]));
+    }
+
+    return result;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: widget.height,
+      child: Stack(children: [
+        CarouselSlider(
+          items: widget.elements,
+          autoPlay: true,
+          enlargeCenterPage: false,
+          viewportFraction: 1.0,
+          height: widget.height,
+          aspectRatio: 16 / 9,
+          pauseAutoPlayOnTouch: Duration(seconds: 5),
+          onPageChanged: (index) {
+            setState(() {
+              _current = index;
+            });
+          },
+        ),
+        Positioned(
+          left: 0,
+          right: 0,
+          bottom: 0,
+          child: Padding(
+            padding: const EdgeInsets.all(1.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: map<Widget>(
+                widget.elements,
+                    (index, url) {
+                  return Container(
+                    width: 8.0,
+                    height: 8.0,
+                    margin: EdgeInsets.symmetric(
+                        vertical: 10.0, horizontal: 2.0),
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: _current == index
+                            ? Color.fromRGBO(255, 255, 255, 0.9)
+                            : Color.fromRGBO(255, 255, 255, 0.4)),
+                  );
+                },
+              ),
+            ),
+          ),
+        ),
+
+      ]),
+    );
+  }
+}
