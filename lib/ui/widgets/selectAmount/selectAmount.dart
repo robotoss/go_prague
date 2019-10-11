91029048c382d8d28445b109021d2dba1814b0bf
robotoss <zoxo@outlook.com>
2019-10-12 00:43:22 +0600

    New Widget

---
+++ lib/ui/widgets/selectAmount/selectAmount.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SelectAmount extends StatefulWidget {
+  @override
+  _SelectAmountState createState() => _SelectAmountState();
+}
+
+class _SelectAmountState extends State<SelectAmount> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
