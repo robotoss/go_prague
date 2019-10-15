5c9028d088d45eb683b9c69167fe2174c61f0b59
robotoss <zoxo@outlook.com>
2019-10-15 19:00:58 +0600

    Change design

--- lib/ui/widgets/buttons/mainCategoryTile.dart
+++ lib/ui/widgets/buttons/mainCategoryTile.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class MainCategoryTile extends StatefulWidget {
   final Color titleColor;
   final String name;
+  final String icon;
 
   MainCategoryTile({
     this.titleColor,
     this.name,
+    this.icon,
 
 });
 
@@ -17,23 +20,34 @@ class MainCategoryTile extends StatefulWidget {
 class _MainCategoryTileState extends State<MainCategoryTile> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 120,
-      height: 150,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.all(Radius.circular(6)),
-        color: widget.titleColor
-      ),
-      child: Column(
-        children: <Widget>[
-          Text(
-              widget.name,
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.white
-            ),
-          )
-        ],
+    return Padding(
+      padding: const EdgeInsets.only(right: 15),
+      child: Container(
+        width: 120,
+        height: 150,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(6)),
+          color: widget.titleColor
+        ),
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: 30,),
+            SvgPicture.asset(widget.icon),
+            SizedBox(height: 10,),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10.0),
+              child: Text(
+                  widget.name,
+                softWrap: true,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
