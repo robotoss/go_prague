974dd70904eb306c120294db13410a39cf7d2158
robotoss <zoxo@outlook.com>
2019-10-19 00:00:09 +0600

    Add transform animation

--- lib/ui/widgets/buttons/mainCategoryTile.dart
+++ lib/ui/widgets/buttons/mainCategoryTile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:go_prague/theme/mainTheme.dart';
 
 class MainCategoryTile extends StatefulWidget {
   final Color titleColor;
@@ -17,36 +18,152 @@ class MainCategoryTile extends StatefulWidget {
   _MainCategoryTileState createState() => _MainCategoryTileState();
 }
 
-class _MainCategoryTileState extends State<MainCategoryTile> {
+class _MainCategoryTileState extends State<MainCategoryTile> with SingleTickerProviderStateMixin{
+
+  Animation<double> animation;
+  AnimationController controller;
+
+  bool isAnimated = true;
+
+  int _statusButton = 0;
+
+
+
+
+
+  @override
+  void initState() {
+    super.initState();
+    controller = new AnimationController(vsync: this,
+        duration: new Duration(seconds: 1));
+    Tween tween = new Tween<double>(begin: 0.0, end: 3.0);
+    animation = tween.animate(controller);
+    animation.addListener(() {
+      setState(() {
+      });
+    });
+  }
+
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(right: 15),
-      child: Container(
-        width: 120,
-        height: 150,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.all(Radius.circular(6)),
-          color: widget.titleColor
-        ),
-        child: Column(
-          children: <Widget>[
-            SizedBox(height: 30,),
-            SvgPicture.asset(widget.icon),
-            SizedBox(height: 10,),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10.0),
-              child: Text(
-                  widget.name,
-                softWrap: true,
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.white
+      child: Transform(
+        transform: Matrix4.identity()..setEntry(3, 2, 0.001)..rotateY(animation.value),
+        alignment: FractionalOffset.center,
+        child: GestureDetector(
+          onTap: (){
+            print('Start animation');
+           if(isAnimated) {
+             isAnimated = false;
+             controller.forward();
+             Future.delayed(const Duration(milliseconds: 500), () {
+               setState(() {
+                 _statusButton = 1;
+               });
+
+             });
+
+             Future.delayed(const Duration(seconds: 5), () {
+               controller.reverse();
+               isAnimated = true;
+               Future.delayed(const Duration(milliseconds: 500), () {
+                 setState(() {
+                   _statusButton = 0;
+                 });
+
+               });
+             });
+           }
+          },
+          child: Container(
+            width: 120,
+            height: 150,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(6)),
+              color: widget.titleColor
+            ),
+            child: _statusButton == 0 ? Column(
+              children: <Widget>[
+                SizedBox(height: 30,),
+                SvgPicture.asset(widget.icon),
+                SizedBox(height: 10,),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                  child: Text(
+                      widget.name,
+                    softWrap: true,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white
+                    ),
+                  ),
+                )
+              ],
+            ) : Column(
+              children: <Widget>[
+                GestureDetector(
+                  onTap: (){},
+                  child: Container(
+                    height: 70,
+                    width: 120,
+                    color: Colors.transparent,
+                    child: Center(
+                      child: Transform(  // Transform widget
+                        transform: Matrix4.identity()
+                          ..setEntry(3, 2, 0.001) // perspective
+                          ..rotateY(3.0),
+                        alignment: FractionalOffset.center,
+                        child: Text(
+                          'In Hotel',
+                          style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.white
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
                 ),
-              ),
-            )
-          ],
+                Container(
+                  height: 10,
+                  width: 120,
+                  color: widget.titleColor == ColorPalette().mainBlue ? ColorPalette().mainGreen : ColorPalette().mainBlue,
+                ),
+                GestureDetector(
+                  onTap: (){},
+                  child: Container(
+                    height: 70,
+                    width: 120,
+                    color: Colors.transparent,
+                    child: Center(
+                      child: Transform(  // Transform widget
+                        transform: Matrix4.identity()
+                          ..setEntry(3, 2, 0.001) // perspective
+                          ..rotateY(3.0),
+                        alignment: FractionalOffset.center,
+                        child: Text(
+                          'In City',
+                          style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.white
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
