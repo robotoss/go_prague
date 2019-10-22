336a1e07e257b2f3c2de661f996fb4a97cb46ae8
robotoss <zoxo@outlook.com>
2019-10-22 15:36:46 +0600

    Change Design

--- lib/ui/restaurant/hotel/productScreen/ProductRestaurantScreen.dart
+++ lib/ui/restaurant/hotel/productScreen/ProductRestaurantScreen.dart
@@ -1,38 +1,31 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:go_prague/data/models/toursData.dart';
+import 'package:go_prague/data/models/hotelRestaurantItems.dart';
 import 'package:go_prague/theme/mainTheme.dart';
 import 'package:go_prague/ui/widgets/buttons/standartButtons.dart';
-import 'package:go_prague/ui/widgets/carousel/carousel.dart';
 import 'package:go_prague/ui/widgets/selectAmount/selectAmount.dart';
 
-class TourInfoScreen extends StatefulWidget {
-  final ToursData tourData;
+class ProductRestaurantScreen extends StatefulWidget {
+  final CategoryItem categoryItem;
 
-  TourInfoScreen({
-    this.tourData,
+  ProductRestaurantScreen({
+    this.categoryItem,
   });
 
   @override
-  _TourInfoScreenState createState() => _TourInfoScreenState();
+  _ProductRestaurantScreenState createState() => _ProductRestaurantScreenState();
 }
 
-class _TourInfoScreenState extends State<TourInfoScreen> {
-
-  static List<Widget> elements = List();
+class _ProductRestaurantScreenState extends State<ProductRestaurantScreen> {
 
   @override
   void initState() {
     super.initState();
-
-    widget.tourData.imgUrls.forEach((image){
-      elements.add(carouselItem(image));
-    });
   }
 
 
   @override
   void dispose() {
-    elements.clear();
     super.dispose();
   }
 
@@ -43,8 +36,9 @@ class _TourInfoScreenState extends State<TourInfoScreen> {
         children: <Widget>[
           Container(
             height: 300,
+            width: double.infinity,
             color: ColorPalette().mainBlack,
-            child: InfoCarousel(height: 300, elements: elements,),
+            child:Image.network(widget.categoryItem.imageUrl, fit: BoxFit.cover, ),
           ),
           Column(
             children: <Widget>[
@@ -74,157 +68,148 @@ class _TourInfoScreenState extends State<TourInfoScreen> {
               children: <Widget>[
 
                 SizedBox(height: 255),
-                Container(
-                  height: 135,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: new BorderRadius.all(const Radius.circular(5.0))
-                  ),
-                  child:
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        Expanded(
-                          flex: 8,
-                          child: Column(
+                Expanded(
+                  child: Container(
+                  height: double.infinity,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: new BorderRadius.all(const Radius.circular(10.0))
+                    ),
+                    child:
+                    Padding(
+                      padding: const EdgeInsets.only(left: 18, right: 18, top: 20),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Row(
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            crossAxisAlignment: CrossAxisAlignment.start,
                             children: <Widget>[
                               Text(
-                                widget.tourData.name,
-                                softWrap: true,
-                                maxLines: 2,
+                                widget.categoryItem.itemName,
                                 style: TextStyle(
-                                    fontSize: 24,
-                                    fontWeight: FontWeight.w700
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.w700
                                 ),
                               ),
                               Row(
                                 children: <Widget>[
                                   Text(
-                                    'Number of tourists',
+                                    widget.categoryItem.price.toString(),
                                     style: TextStyle(
-                                      color: ColorPalette().textLLDark,
-                                      fontSize: 18,
+                                        fontSize: 24,
                                     ),
                                   ),
-                                  SizedBox(width: 15,),
-                                  SelectAmount(),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                        Expanded(
-                          flex: 2,
-                          child: Padding(
-                            padding: const EdgeInsets.only(left: 10),
-                            child: Container(
-                              height: 72,
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: <Widget>[
+                                  SizedBox(width: 10),
                                   Text(
-                                    '${widget.tourData.duration}',
+                                    'Kč',
                                     style: TextStyle(
-                                        color: ColorPalette().textLLDark,
-                                        fontSize: 22,
-                                        fontWeight: FontWeight.w700
-                                    ),
-                                  ),
-                                  Text(
-                                    widget.tourData.price == 0 ? 'FREE' : '${widget.tourData.price} Kč',
-                                    style: TextStyle(
-                                        color: ColorPalette().textLLDark,
-                                        fontSize: 22,
+                                        fontSize: 24,
                                         fontWeight: FontWeight.w700
                                     ),
                                   ),
                                 ],
-                              ),
-                            ),
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-                SizedBox(height: 20,),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.only(bottom: 20),
-                    child: Container(
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: new BorderRadius.all(const Radius.circular(5.0))
-                      ),
-                      child:
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
-                        child: SingleChildScrollView(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: <Widget>[
-                              ConstrainedBox(
-                                constraints: BoxConstraints(
-                                    minHeight: 250
-                                ),
-                                child: Text(
-                                  widget.tourData.article,
-                                  style: TextStyle(
-                                    fontSize: 18,
-                                    color: ColorPalette().textLLDark,
-                                  ),
-                                ),
-                              ),
-                              SizedBox(height: 15),
-                              Container(
-                                child: Column(
-                                  children: <Widget>[
-                                    Row(
-                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                      children: <Widget>[
-                                        Text(
-                                          'Select a day',
-                                          style: TextStyle(
-                                              color: ColorPalette().textLLDark,
-                                              fontSize: 22,
-                                              fontWeight: FontWeight.w700
-                                          ),
-                                        ),
-                                        Text('TODAY'),
-                                      ],
-                                    ),
-                                    SizedBox(height: 25),
-                                    Row(
-                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                      children: <Widget>[
-                                        Text(
-                                          'Select a time',
-                                          style: TextStyle(
-                                              color: ColorPalette().textLLDark,
-                                              fontSize: 22,
-                                              fontWeight: FontWeight.w700
-                                          ),
-                                        ),
-                                        Text('16:10'),
-                                      ],
-                                    ),
-                                    SizedBox(height: 40),
-                                    AddToCartButton(),
-                                    SizedBox(height: 5,)
-                                  ],
-                                ),
                               )
                             ],
                           ),
-                        ),
+                          SizedBox(height: 30),
+                          Divider(height: 1, color: ColorPalette().textLightDark,),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 10),
+                            child: Text(
+                              widget.categoryItem.description,
+                              maxLines: 4,
+                              style: TextStyle(
+                                color: ColorPalette().textLightDark,
+                                fontSize: 18,
+                              ),
+                            ),
+                          ),
+                          Divider(height: 1, color: ColorPalette().textLightDark,),
+                          ListView.builder(
+                            shrinkWrap: true,
+                            scrollDirection: Axis.vertical,
+                            itemCount: widget.categoryItem.parametrs.length,
+                              itemBuilder: (context, index){
+                              return Padding(
+                                padding: const EdgeInsets.symmetric(vertical: 10),
+                                child: Row(
+                                  children: <Widget>[
+                                    Text(
+                                        widget.categoryItem.parametrs[index].parametrName,
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                        color: ColorPalette().textLightDark
+                                      ),
+                                    ),
+                                    Expanded(
+                                      child: Container(
+                                        height: 30,
+                                        child: ListView.builder(
+                                            shrinkWrap: true,
+                                            scrollDirection: Axis.horizontal,
+                                            itemCount: widget.categoryItem.parametrs[index].parametrsItems.length,
+                                            itemBuilder: (context,i) {
+                                              return MeatButton(active: false, name: widget.categoryItem.parametrs[index].parametrsItems[i],);
+                                            }
+                                        ),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              );
+                              }),
+                          Divider(height: 1, color: ColorPalette().textLightDark,),
+                          Expanded(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                SizedBox(height: 10,),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: <Widget>[
+                                    Row(
+                                      children: <Widget>[
+                                        Icon(Icons.access_time),
+                                        SizedBox(width: 5),
+                                        Text(
+                                          widget.categoryItem.cookTime.toString(),
+                                          style: TextStyle(
+                                            fontSize: 22
+                                          ),
+                                        ),
+                                        Text(
+                                          ' min',
+                                          style: TextStyle(
+                                              fontSize: 22,
+                                            fontWeight: FontWeight.w700
+                                          ),
+                                        ),
+
+                                      ],
+                                    ),
+                                    Row(
+                                      children: <Widget>[
+                                        Text(
+                                          'Quantity',
+                                          style: TextStyle(
+                                              fontSize: 18
+                                          ),
+                                        ),
+                                        SizedBox(width: 10,),
+                                        SelectAmount(),
+                                      ],
+                                    )
+                                  ],
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(bottom: 35),
+                                  child: AddToCartButton(),
+                                )
+                              ],
+                            ),
+                          )
+                        ],
                       ),
                     ),
                   ),
