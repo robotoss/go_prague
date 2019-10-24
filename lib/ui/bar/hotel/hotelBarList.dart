15aa32b71ddc7c9d2397d223b71c84226ac4edbb
robotoss <zoxo@outlook.com>
2019-10-24 12:40:27 +0600

    Add Bar List Screen

---
+++ lib/ui/bar/hotel/hotelBarList.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:go_prague/data/models/hotelBarItems.dart';
+import 'package:go_prague/data/repository.dart';
+import 'package:go_prague/theme/mainTheme.dart';
+
+import 'productCard/hotelBarProductCardScreen.dart';
+
+class HotelBarList extends StatelessWidget {
+
+  static List<HotelBartItems> _hotelBarItems = List();
+
+  Future<List<HotelBartItems>> getBarData() async{
+    await Repository().hotelBarList().then((response){
+      _hotelBarItems = response;
+    });
+
+    return _hotelBarItems;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back_ios, color: Colors.black,),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+          title: Text(
+            'Hotel Bar',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.w700, color: Colors.black,),
+          ),
+          centerTitle: true,
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.shopping_cart, color: Colors.black,),
+              onPressed: () {
+              },
+            ),
+          ],
+        ),
+        body: FutureBuilder(
+          future: getBarData(),
+          builder: (context, snapshot) {
+            return snapshot.data ==null ? Center(child: CircularProgressIndicator(),) : DefaultTabController(
+                length: _hotelBarItems.length,
+                child: Column(
+                  children: <Widget>[
+                    TabBar(
+                        labelPadding: EdgeInsets.only(left: 10,right: 10),
+                        isScrollable: true,
+                        indicatorColor: ColorPalette().mainGreen,
+                        labelStyle: TextStyle(
+                          fontSize: 28,
+                        ),
+                        labelColor: ColorPalette().mainBlack,
+                        tabs: List<Widget>.generate(_hotelBarItems.length, (int index){
+                          return Tab(text: _hotelBarItems[index].categoryName);
+
+                        })
+                    ),
+                    Expanded(
+                      child: TabBarView(
+                        children: List<Widget>.generate(_hotelBarItems.length, (int index){
+                          return Container(
+                            child: listCards(_hotelBarItems[index].categoryItems),
+                          );
+                        }),
+                      ),
+                    )
+                  ],
+                ));
+          },
+        )
+    );
+  }
+
+  Widget listCards(List<CategoryItem> itemsData) {
+    print('Count ${itemsData.length}');
+    return ListView.builder(
+        shrinkWrap: true,
+        scrollDirection: Axis.vertical,
+        itemCount: itemsData.length,
+        itemBuilder: (context, index){
+          return HotelBarProductCardScreen(
+            categoryItem: itemsData[index],
+          );
+        }
+    );
+  }
+}
---
+++ lib/ui/bar/hotel/productCard/hotelBarProductCardScreen.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:go_prague/data/models/hotelBarItems.dart';
+import 'package:go_prague/theme/mainTheme.dart';
+import 'package:go_prague/ui/restaurant/hotel/productScreen/ProductRestaurantScreen.dart';
+import 'package:go_prague/ui/widgets/buttons/standartButtons.dart';
+import 'package:go_prague/ui/widgets/selectAmount/selectAmount.dart';
+
+class HotelBarProductCardScreen extends StatefulWidget {
+  final CategoryItem categoryItem;
+
+  HotelBarProductCardScreen({
+    this.categoryItem
+  });
+
+
+  @override
+  _HotelBarProductCardScreenState createState() => _HotelBarProductCardScreenState();
+}
+
+class _HotelBarProductCardScreenState extends State<HotelBarProductCardScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Card(
+        child: Container(
+          height: 250,
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                height: 125,
+                width: double.infinity,
+                child: Image.network(widget.categoryItem.imageUrl, fit: BoxFit.cover,),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 8, left: 15, right: 15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      widget.categoryItem.itemName,
+                      style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.w700
+                      ),
+                    ),
+                    Text(
+                      widget.categoryItem.description,
+                      softWrap: true,
+                      maxLines: 2,
+                      style: TextStyle(
+                          fontSize: 16,
+                          color: ColorPalette().textLightDark
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Text(
+                          '~${ widget.categoryItem.time} min',
+                          style: TextStyle(
+                              fontSize: 20,
+                              color: ColorPalette().textLightDark
+                          ),
+                        ),
+                        SizedBox(width: 15),
+                        Text(
+                          '${widget.categoryItem.price} Kč',
+                          style: TextStyle(
+                              fontSize: 20,
+                              color: ColorPalette().textLightDark
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: <Widget>[
+                        SelectAmount(),
+                        SizedBox(width: 10,),
+                        AddToCartItemButton(),
+                        SizedBox(width: 10,),
+                        MoreBlueButton(),
+                      ],
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
