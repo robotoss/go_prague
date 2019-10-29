e34334fac12e28af68457cd84ff31ba66fc5c42f
robotoss <zoxo@outlook.com>
2019-10-30 01:02:12 +0600

    Add provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:go_prague/ui/homeScreen/homeScreen.dart';
+import 'package:provider/provider.dart';
 
+import 'data/bloc/cart_bloc.dart';
 import 'theme/mainTheme.dart';
 
 void main() => runApp(MyApp());
@@ -8,10 +10,13 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: defaultTheme,
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+    return ChangeNotifierProvider<CartBloc>(
+      builder: (context) => CartBloc(),
+      child: MaterialApp(
+        theme: defaultTheme,
+        debugShowCheckedModeBanner: false,
+        home: HomeScreen(),
+      ),
     );
   }
 }
