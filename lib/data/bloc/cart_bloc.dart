91db3ecbfe60d00717c97b9d8d231d99c92f28dd
robotoss <zoxo@outlook.com>
2019-11-01 12:14:32 +0600

    Add two new model to cart

--- lib/data/bloc/cart_bloc.dart
+++ lib/data/bloc/cart_bloc.dart
@@ -5,11 +5,17 @@ class CartBloc with ChangeNotifier {
   List<RestaurantItems> _restaurantItems = [];
   List<RestaurantItems> get restaurantItems => _restaurantItems;
 
+  List<BarItems> _barItems = [];
+  List<BarItems> get barItems => _barItems;
+
+  List<TourItems> _tourItems = [];
+  List<TourItems> get tourItems => _tourItems;
+
   Map<int, int> _cart = {};
 
   Map<int, int> get cart => _cart;
 
-  void addToCart(RestaurantItem item) {
+  void addToCartRestaurant(RestaurantItem item) {
     _restaurantItems.add(item);
     print('Add to cart');
     print(_restaurantItems.length);
@@ -21,11 +27,41 @@ class CartBloc with ChangeNotifier {
     notifyListeners();
   }
 
-  void clear(index) {
-    if (_cart.containsKey(index)) {
-      _cart.remove(index);
-      notifyListeners();
+  void addToCartBar(BarItem item) {
+    _barItems.add(item);
+    print('Add to cart');
+    print(_restaurantItems.length);
+//    if (_cart.containsKey(index)) {
+//      _cart[index] += 1;
+//    } else {
+//      _cart[index] = 1;
+//    }
+    notifyListeners();
+  }
+
+  void addToCartTour(TourItem item) {
+    _tourItems.add(item);
+    print('Add to cart');
+    print(_restaurantItems.length);
+//    if (_cart.containsKey(index)) {
+//      _cart[index] += 1;
+//    } else {
+//      _cart[index] = 1;
+//    }
+    notifyListeners();
+  }
+
+  void clear(String type, index) {
+    print('Remove from cart');
+    if(type == 'Restaurant'){
+      _restaurantItems.removeAt(index);
     }
+    notifyListeners();
+    print('Cart count - ${_restaurantItems.length}');
+//    if (_cart.containsKey(index)) {
+//      _cart.remove(index);
+//      notifyListeners();
+//    }
   }
 }
 
@@ -35,6 +71,29 @@ class RestaurantItem implements RestaurantItems {
   final String nameRestaurant;
   final String name;
   final int price;
+  final String type;
 
-  RestaurantItem(this.nameRestaurant, this.name, this.price,);
+  RestaurantItem(this.nameRestaurant, this.name, this.price, this.type);
+}
+
+
+abstract class BarItems {}
+
+class BarItem implements BarItems {
+  final String nameBar;
+  final String name;
+  final int price;
+  final String type;
+
+  BarItem(this.nameBar, this.name, this.price, this.type);
+}
+
+abstract class TourItems {}
+
+class TourItem implements TourItems {
+  final String nameTour;
+  final int price;
+  final String type;
+
+  TourItem(this.nameTour, this.price, this.type);
 }
