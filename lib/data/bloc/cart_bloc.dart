51d5065a77a78dcc648b3ee9fbfae57342b5c4a2
robotoss <zoxo@outlook.com>
2019-11-15 00:54:50 +0600

    Clean code

--- lib/data/bloc/cart_bloc.dart
+++ lib/data/bloc/cart_bloc.dart
@@ -6,51 +6,26 @@ class CartBloc with ChangeNotifier {
   List<CartItem> get cartItems => _cartItems;
 
 
-
-//  Map<int, int> _cart = {};
-//
-//  Map<int, int> get cart => _cart;
-
   void addToCart(CartItem item) {
-    print('Этап 1');
-
     bool isItemFound = false;
-
     _cartItems.forEach((f){
-      print('Этап 2');
       if(item.name == f.name && item.placeName == f.placeName) {
         isItemFound = true;
         f.quantity = f.quantity + item.quantity;
         f.price = (f.price + item.price);
       }
     });
-    print('Этап 3');
-
     if(!isItemFound){
       item.price = item.price * item.quantity;
       _cartItems.add(item);
     }
-
-
-
-//    if (_cart.containsKey(index)) {
-//      _cart[index] += 1;
-//    } else {
-//      _cart[index] = 1;
-//    }
     notifyListeners();
   }
-
 
   void clear(int index) {
     _cartItems.removeAt(index);
 
     notifyListeners();
-
-//    if (_cart.containsKey(index)) {
-//      _cart.remove(index);
-//      notifyListeners();
-//    }
   }
 
   void changeQuantity (int index,  bool plus ) {
